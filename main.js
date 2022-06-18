7b5d3f12f7ef37b5bbe0891a5e7e29af19f7485d
sandeepsinghvirdi <sandeep_sonu07@yahoo.com>
2022-06-18 11:31:32 +1000

    Starting coding functions in js file

--- main.js
+++ main.js
@@ -10,7 +10,13 @@ fetch("http://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid
         for(i=0; i<5; i++){
             document.getElementById("day" +(i+1)+"Min").innerHTML = "Min:" +Number(data.list[i].main.temp_min -288.53).toFixed(1)+"°";     
           }
-          for(i=0; i<5; i++){
-            document.getElementById("day" +(i+1)+"Max").innerHTML = "Min:" +Number(data.list[i].main.temp_max -288.53).toFixed(1)+"°";
+        for(i=0; i<5; i++){
+            document.getElementById("day" +(i+1)+"Max").innerHTML = "Max:" +Number(data.list[i].main.temp_max -288.53).toFixed(1)+"°";
           }
+        
+        for(i=0; i<5; i++){
+            document.getElementById("img" +(i++)).src = "http://openweathermap.org/img/wn/" +data.list[i].weather[0].icon+".png";
+        }
+
+
     } )
