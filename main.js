503464a9d939bb35cf579cb83d2f063ef5d37b44
sandeepsinghvirdi <sandeep_sonu07@yahoo.com>
2022-07-02 17:40:31 +1000

    Added code for default geolocation

--- main.js
+++ main.js
@@ -1,7 +1,57 @@
+const newName = document.getElementById("cityInput");
+const cityName = document.getElementById("cityName");
 
-function getInfo(){
-    const newName = document.getElementById("cityInput");
-    const cityName = document.getElementById("cityName");
+
+window.addEventListener("load" ,() =>{
+    if(navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        alert("Your browser does not support geolocation api");
+    }
+    });
+    
+    
+    
+    function onSuccess(position){
+        const { latitude, longitude } = position.coords;
+       
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=399742cdcb44f520879454415508fa74`)
+        
+            .then(response => response.json())
+            
+            .then(data =>{
+                console.log(data);
+             document.getElementById(cityName.innerHTML="--"+data.city.name+"--");
+                for(i=0; i<5; i++){
+                    document.getElementById("day" +(i+1)+"Min").innerHTML = "Min:" +Number(data.list[i].main.temp_min -288.53).toFixed(1)+"°";     
+                  }
+                for(i=0; i<5; i++){
+                    document.getElementById("day" +(i+1)+"Max").innerHTML = "Max:" +Number(data.list[i].main.temp_max -288.53).toFixed(1)+"°";
+                  }
+                
+                for(i=0; i<5; i++){
+                    document.getElementById("img" +(i+1)).src = "https://openweathermap.org/img/wn/" +data.list[i].weather[0].icon+".png";
+                }
+              
+            } )
+        
+        
+       
+    
+        .catch(err => alert("Something went wrong"));
+         
+        }
+    
+    
+    function onError(error){
+        return "wrong city";
+      }
+
+    cityInputBox=document.getElementById("cityInput");
+    cityInputBox.addEventListener(onfocus, function refresh(){ return cityName.innerHTML="----"} );
+    function getInfo(){
+    
     cityName.innerHTML = "--"+newName.value+"--";
 
     fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=399742cdcb44f520879454415508fa74')
@@ -20,11 +70,6 @@ function getInfo(){
     } )
 
 .catch(err => alert("Something went wrong"));
-}
-
-function defaultScreen(){
-    document.getElementById("cityInput").defaultValue = "Melbourne";
-    getInfo();
 }
 
 
