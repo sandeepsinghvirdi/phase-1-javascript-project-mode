eb6cbcc7469a848d17e8c0bfcd2c496d0ac40a46
sandeepsinghvirdi <sandeep_sonu07@yahoo.com>
2022-07-13 15:58:08 +1000

    Added code for geolocation

--- main.js
+++ main.js
@@ -3,19 +3,25 @@ const cityName = document.getElementById("cityName");
 
 
 window.addEventListener("load" ,() =>{
+    document.getElementById("iconsContainer").style.visibility = 'visible';
+    document.getElementById("weatherContainer").style.visibility = 'hidden';
+
     if(navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    }else{
-        alert("Your browser does not support geolocation api");
+        {
+            navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        }   else{
+            alert("Your browser does not support geolocation api");
+                }
     }
-    });
+    );
     
     
     
     function onSuccess(position){
         const { latitude, longitude } = position.coords;
-       
+        document.getElementById("iconsContainer").style.visibility = 'visible';
+        document.getElementById("weatherContainer").style.visibility = 'hidden';
+  
         fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=399742cdcb44f520879454415508fa74`)
         
             .then(response => response.json())
@@ -24,54 +30,72 @@ window.addEventListener("load" ,() =>{
                 console.log(data);
              document.getElementById(cityName.innerHTML="--"+data.city.name+"--");
                 for(i=0; i<5; i++){
-                    document.getElementById("day" +(i+1)+"Min").innerHTML = "Min:" +Number(data.list[i].main.temp_min -288.53).toFixed(1)+"°";     
+                    document.getElementById("day" +(i+1)+"Min").innerHTML = "Min:" +Number(data.list[i].main.temp_min -272.15).toFixed(1)+"°";     
                   }
                 for(i=0; i<5; i++){
-                    document.getElementById("day" +(i+1)+"Max").innerHTML = "Max:" +Number(data.list[i].main.temp_max -288.53).toFixed(1)+"°";
+                    document.getElementById("day" +(i+1)+"Max").innerHTML = "Max:" +Number(data.list[i].main.temp_max -272.15).toFixed(1)+"°";
                   }
                 
                 for(i=0; i<5; i++){
                     document.getElementById("img" +(i+1)).src = "https://openweathermap.org/img/wn/" +data.list[i].weather[0].icon+".png";
                 }
               
-            } )
-        
-        
+            })
+    
+    }
+
+    
+    function onError(err)
+    {
+        let weatherContainer = document.getElementById("weatherContainer");
+        weatherContainer.style.visibility='visible';
+        let errorHandle=document.createElement("h1");
+        errorHandle.innerHTML="Geoloaction not supported";
+        weatherContainer.appendChild(errorHandle).style.textAlign = 'center';
+        document.getElementById("iconsContainer").style.visibility = 'hidden';
+     
+
+    }
+     
        
-    
-        .catch(err => alert("Something went wrong"));
-         
-        }
-    
-    
-    function onError(error){
-        return "wrong city";
-      }
-
-    cityInputBox=document.getElementById("cityInput");
-    cityInputBox.addEventListener(onfocus, function refresh(){ return cityName.innerHTML="----"} );
     function getInfo(){
-    
-    cityName.innerHTML = "--"+newName.value+"--";
 
+    document.getElementById("iconsContainer").style.visibility = 'visible';
+    document.getElementById("weatherContainer").style.visibility = 'hidden';
+    cityName.innerHTML = "--"+newName.value+"--";
+    
     fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=399742cdcb44f520879454415508fa74')
     .then(response => response.json())
     .then(data =>{
+       
         for(i=0; i<5; i++){
-            document.getElementById("day" +(i+1)+"Min").innerHTML = "Min:" +Number(data.list[i].main.temp_min -288.53).toFixed(1)+"°";     
+            document.getElementById("day" +(i+1)+"Min").innerHTML = "Min:" +Number(data.list[i].main.temp_min -272.15).toFixed(1)+"°";     
           }
         for(i=0; i<5; i++){
-            document.getElementById("day" +(i+1)+"Max").innerHTML = "Max:" +Number(data.list[i].main.temp_max -288.53).toFixed(1)+"°";
+            document.getElementById("day" +(i+1)+"Max").innerHTML = "Max:" +Number(data.list[i].main.temp_max -272.15).toFixed(1)+"°";
           }
         
         for(i=0; i<5; i++){
             document.getElementById("img" +(i+1)).src = "https://openweathermap.org/img/wn/" +data.list[i].weather[0].icon+".png";
         }
-    } )
+       
+    })
 
-.catch(err => alert("Something went wrong"));
-}
+    .catch(err => {
+        
+        let weatherContainer = document.getElementById("weatherContainer");
+            weatherContainer.style.visibility='visible';
+        let errorHandle=document.createElement("h1");
+            errorHandle.innerHTML="Enter a valid city name";
+            weatherContainer.appendChild(errorHandle).style.textAlign = 'center';
+            document.getElementById("iconsContainer").style.visibility = 'hidden';
+                newName.value=""; 
+       
+                
+})
 
+    }
+   
 
 const d = new Date();
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
